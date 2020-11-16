86dd42428fb6955dc48a9ab8177d406547a08ceb
JadeMillar <jmillar2@wisc.edu>
2020-11-16 16:57:39 -0600

    november 16 2020

---
+++ Comments/commentDrills.py
@@ -0,0 +1,50 @@
+'''
+For this assignment you should read the task, then below the task do what 
+it asks you to do.
+EXAMPLE TASK:
+'''
+#EX) Make a single line comment
+
+#This is a single line comment
+
+'''
+END OF EXAMPLE
+'''
+
+'''
+START HERE
+'''
+
+#1) Make a single line comment.
+
+#hey this isnt working 
+
+#2) Make a multi-line comment.
+
+'''
+hello
+this 
+is 
+a multi
+line
+comment
+'''
+
+#3) Make a variable. Then put a comment after it, on the same line.
+
+var2=a #coding is cool
+
+#4) Make a single line comment.
+
+#hey its jade, how are you doing
+
+#5) Make a multi-line comment.
+
+'''
+my least
+favorite subject
+is
+math because
+it is so
+challenging
+'''
