4a44d4ffbcb4129e880e03848ed7b79b58c8fa60
JadeMillar <jmillar2@wisc.edu>
2020-11-16 16:49:41 -0600

    november 16 2020

---
+++ Comments/commentsOutline.py
@@ -0,0 +1,26 @@
+'''
+This outline will help solidify concepts from the Comments lesson.
+Fill in this outline as the instructor goes through the lesson.
+'''
+
+#1) Make a single line comment.
+
+#ajdfkjdfjdsf 
+
+
+
+#2) Make a multi-line comment.
+
+'''
+ajkdfjakfk
+ajdfdjkfjd
+fdjkfkjd
+fkdjfd
+
+'''
+#3) Make an int variable. Then, make a single line comment on the 
+#same line, after the variable.
+
+var=4 #singlelinecomment
+print(var)
+
