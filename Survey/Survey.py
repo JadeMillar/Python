849e8d62af0a65c52c92e0995016f43ce662f7f7
JadeMillar <jmillar2@wisc.edu>
2020-12-08 21:26:50 -0600

    december 8

---
+++ Survey/Survey.py
@@ -0,0 +1,40 @@
+'''
+The Objective is to make a program that can take survey information
+from a user.
+
+
+@author: Jade Millar
+'''
+#Use input() to ask the user "What is your name?" and store that in
+#a variable called name. Then print the variable.
+question="What is your name?"
+response=input(question)
+name=response 
+print(name)
+#Use input() to ask the user "What is your favorite color?" and store that 
+#in a variable called color. Then print the variable.
+question="What is your favorite color?"
+response=input(question)
+color=response
+print(color)
+
+#Use input() to ask the user "What city did you grow up in?" and store 
+#that in a variable called city. Then print the variable.
+question="What city did you grow up in?"
+response=input(question)
+city=response
+print(city)
+
+#Use input() to ask the user "What is your best friends name?" and store 
+#that in a variable called friend. Then print the variable.
+question="What is your best friends name?"
+response=input(question)
+friend=response
+print(friend)
+
+#Use input() to ask the user "What color hair do you have?" and store that 
+#in a variable called hair. Then print the variable.
+question="What color hair do you have?"
+response=input(question)
+hair=response
+print(hair)
