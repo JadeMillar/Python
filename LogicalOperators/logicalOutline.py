7298be042a242626d87bd7c72d2b3ad9ccf49a83
JadeMillar <jmillar2@wisc.edu>
2021-02-23 09:53:06 -0600

    feb 23

---
+++ LogicalOperators/logicalOutline.py
@@ -0,0 +1,39 @@
+'''
+This outline will help solidify concepts from the Logical Operators lesson.
+Fill in this outline as the instructor goes through the lesson.
+'''
+#EX) Make two boolean variables. Put them on either side of the and operator.
+#Store this expression in a variable named a. Print the variable.
+one = True
+two = False
+a = one and two 
+print(a)
+
+#1) Make two boolean variables. Put them on either side of the and operator.
+#Store this expression in a variable named a. Print the variable.
+#true     #true
+a= 4==4 and 5==5
+#true
+print(a)
+
+#2) Make two boolean variables. Put them on either side of the or operator.
+#Store this expression in a variable named b. Print the variable.
+
+#false     #true
+b= 2==4 or 5==5
+#true 
+print(b)
+
+#3) Make one boolean variable. Put the variable after the not. Store this 
+#expression in a variable named c. Print the variable.
+
+c= not 4==5
+#true
+print(c)
+
+
+#4) Make a logical expression with one of the common SYNTAX errors.
+
+not 
+
+boolean=(username=="jade") or (password=="miljadsd")
---
+++ conversions/conversion.py
@@ -0,0 +1,33 @@
+'''
+The objective is to have the user input a number of miles.
+
+the number of miles will be converted to yards, feet, and inches.
+
+The conversions are then printed out with a simple statement.
+
+@Author: Jade Millar
+'''
+
+#Use input() to get the number of miles from the user. And store
+#that int in a variable called miles.
+input(userResponse)
+#Convert miles to yards, using the following:
+# 1 mile = 1760 yards.
+#
+#Store the value in a variable called yards and print it out with a 
+#simple statement.
+yards= miles * 1760
+miles= 1
+yards= 1760
+print(str(miles) + "converts to" + str(yards) + "yards.")
+#Convert miles to feet, using the following:
+# 1 mile = 5280 feet.
+#
+#Store the value in a variable called feet and print it out with a 
+#simple statement.
+
+#Convert miles to inches, using the following:
+# 1 mile = 63,360 inches.
+#
+#Store the value in a variable called inches and print it out with a 
+#simple statement.
