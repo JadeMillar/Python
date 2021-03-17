8f462fff7542100f7d7e2dcadb627949b1093808
JadeMillar <jmillar2@wisc.edu>
2021-03-17 18:13:06 -0500

    jade

---
+++ conditions/project.py
@@ -0,0 +1,56 @@
+'''
+Created on Mar 17, 2021
+
+@author: jade Millar
+'''
+#Make a variable called score to keep track of the correct answers. And set
+#it to 0.
+
+#Ask the user question one. And store the users answer.
+score= 0 
+question = (" What is the powerhouse of the cell?")
+#Check if the answer is correct, if it is add one to score and print out
+#"Correct"
+#Else, print out "Incorrect, the correct answer is A."
+answer = "a"
+if(answer == a):
+    print ("correct")
+else:
+    print("incorrect, the correct answer is A")
+score = 1
+#Ask the user question two. And store the users answer.
+quesion= ("How many states comprise the United States?")
+#Check if the answer is correct, if it is add one to score and print out
+#"Correct"
+#Else, print out "Incorrect, the correct answer is C."
+answer = "a"
+if(answer == "c"):
+    print("correct")
+else:
+    print("incorrect, the correct answer is C")
+score = 1 
+#Ask the user question three. And store the users answer.
+question = ("In y = mx + b, what does m stand for?")
+#Check if the answer is correct, if it is add one to score and print out
+#"Correct"
+#Else, print out "Incorrect, the correct answer is A."
+answer = "a"
+if(answer == "a"):
+    print("correct")
+else: 
+    print("incorrect, the correct answer is A")
+score = 2 
+#Ask the user question four. And store the users answer.
+question = ("In English, a person, place or thing is called:")
+#Check if the answer is correct, if it is add one to score and print out
+#"Correct"
+#Else, print out "Incorrect, the correct answer is C."
+answer = "c"
+if(answer == "c"):
+    print("correct")
+else:
+    print("incorrect, the correct answer is C")
+#Calculate the percentage the user got. And store it in a variable called p
+p = 75%
+print("you got a 75% or a 3/4")
+#Print out the users score: "You got a [score]/4. Or a [p]%."
