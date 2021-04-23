49872cab575d622b1b666eb7c1dc5f0774bf8332
JadeMillar <jmillar2@wisc.edu>
2021-04-22 22:35:09 -0500

    22

--- loop/functionOutline.py
+++ loop/functionOutline.py
@@ -7,17 +7,39 @@ Fill in this outline as the instructor goes through the lesson.
 #true, return true. Else, return false. Then, call the function.
 #Print what the function returns.
 
-
+def boolean (a,b):
+    if(boolean == True):
+        print("true")
+        return 
+    else:
+        print("false")
+        return 
+    a = True
+    b= False
+    boo = boolean (a,b)
 
 #2) Make a function that takes one int parameter: gallons. Convert gallons
 #to cups (do this by multiplying gallons by 16). Then return cups. Then,
 #call the function.
 #Print what the function returns.
 
-
+def convert(gallons):
+    cups = gallons * 16
+    return cups
+gallons = 5
+cups = convert(gallons)
+print(cups)
 
 #3) Make a function of any any kind with a common SYNTAX error. Then, call the
 #function.
 #Print what the function returns.
-
-
+'''
+This function takes in two integer parameters and adds them together. Then,
+it returns an integer which is the sum of the parameters.
+parameters: two integer variables
+function purpose: sum the parameters together
+returns: an integer sum of the parameters
+'''
+de sumTwoNumbers (numOne, numTwo):
+    sum = numOne - numTwo
+    return sum
